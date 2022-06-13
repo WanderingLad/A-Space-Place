77b50a89d70d34b8d70c6d924056b03a777b8b8c
James Reilly <jreilly2112@gmail.com>
2022-06-13 19:40:12 -0400

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts',
+  process.env.MONGODB_URI || 'mongodb+srv://admin:alphabetsoup@a-space-place.lixpj49.mongodb.net/?retryWrites=true&w=majority',
 
   {
     useNewUrlParser: true,
